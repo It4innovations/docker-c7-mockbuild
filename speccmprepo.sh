faa1f4188ed6565ef15bb59943cae0623fad5d8a
Marek Chrastina <marek.chrastina@vsb.cz>
2018-04-30 13:47:08 +0200

    change from all spec to one src.rpm

--- speccmprepo.sh
+++ speccmprepo.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 
-if ! BRANCH=$(git rev-parse --abbrev-ref HEAD)
-then
+RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}" $1)
+RPM_REMOTE="fail2ban-0.11-2.el7.centos"
+rpmdev-vercmp $RPM_LOCAL $RPM_REMOTE
+if [ $? != 11 ]; then
+  echo "Building $RPM_LOCAL is not newer then package in the remote yum repository."
   exit 1
 fi
-if [ "$BRANCH" == "master" ]; then
-  LIST=$(git log -m -1 --name-only --pretty="format:" | sed '/./,$!d' | awk '/^$/{exit} {print $0}' | sed '/\.spec$/!d')
-else
-  LIST=$(git diff master...$BRANCH --name-only | sed '/\.spec$/!d')
-fi
-for i in $LIST; do
-  RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}" $i)
-  RPM_REMOTE="fail2ban-0.11-2.el7.centos"
-  rpmdev-vercmp $RPM_LOCAL $RPM_REMOTE
-  if [ $? != 11 ]; then
-    echo "Building $RPM_LOCAL is not newer then package in the remote yum repository."
-    exit 1
-  fi
-done
