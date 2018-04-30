d9bc28708fe815759280cdad792a437db2880793
Marek Chrastina <marek.chrastina@vsb.cz>
2018-04-30 14:50:40 +0200

    allow multiple src.rpm in argument

--- speccmprepo.sh
+++ speccmprepo.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}" $1)
-RPM_REMOTE="fail2ban-0.11-2.el7.centos"
-rpmdev-vercmp $RPM_LOCAL $RPM_REMOTE
-if [ $? != 11 ]; then
-  echo "Building $RPM_LOCAL is not newer then package in the remote yum repository."
-  exit 1
-fi
+for file in "$@"
+do
+  RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}.%{ARCH}" $1)
+  RPM_REMOTE="fail2ban-0.11-2.el7.centos.noarch"
+  rpmdev-vercmp $RPM_LOCAL $RPM_REMOTE
+  if [ $? != 11 ]; then
+    echo "Building $RPM_LOCAL is not newer then package in the remote yum repository."
+    exit 1
+  fi
+done
