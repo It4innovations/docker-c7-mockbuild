1bd4a6cc8cbaa2c8f1b0fc4eef3e6322c0c5776a
Marek Chrastina <marek.chrastina@vsb.cz>
2018-05-09 10:46:24 +0200

    change repoquery comparing

--- speccmprepo.sh
+++ speccmprepo.sh
@@ -2,7 +2,7 @@
 
 for file in "$@"; do
   RPM_NAME=$(rpm -qp --queryformat "%{NAME}" "$file")
-  RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$file")
+  RPM_LOCAL=$(rpm -qp --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$file" | sed "s/.centos//")
   BUILDARCH=$(rpm2cpio "$file" | cpio -civ --to-stdout '*.spec' | grep BuildArch | cut -d ":" -f2- | tr -d '[:space:]')
   MOCKTARGETS=$(rpm2cpio "$file" | cpio -civ --to-stdout '*.spec' | grep "#mocktargets:" | cut -d ":" -f2)
   if ! [ -z "$BUILDARCH" ]; then
@@ -15,29 +15,30 @@ for file in "$@"; do
     fi
     MOCKTARGETS=$(echo "$MOCKTARGETS" | grep -Eo "\w+\-[0-9]+\-noarch" | sort | uniq)
   fi
-
   for target in $MOCKTARGETS; do
     RELEASESERVER=$(echo "$target" | grep -Eo "\-[0-9]+\-" | grep -Eo "[0-9]+")
+    DIST=$(echo "$target" | grep -Eo "^\w+")
     ARCH=$(echo "$target" | grep -Eo "\w+$")
-    RPM_REMOTE=$(repoquery --pkgnarrow=all --releaseserver="$RELEASESERVER" --archlist="$ARCH" --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$RPM_NAME"."$ARCH")
+    REPOPATH="http://repository.it4i.cz/it4irepo/$DIST/$RELEASESERVER/stable/"
+    RPM_REMOTE=$(repoquery --pkgnarrow=all --archlist=src --repofrompath=it4irepo,"$REPOPATH"SRPMS/ --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$RPM_NAME")
+    if [ -z "$RPM_REMOTE" ]; then
+      echo "Brand new source package $RPM_LOCAL"
+    else
+      rpmdev-vercmp "$RPM_LOCAL" "$RPM_REMOTE"
+      if [ $? != 11 ]; then
+        echo "Building source package $RPM_LOCAL is not newer then package in the remote yum repository."
+        exit 1
+      fi
+    fi
+    RPM_REMOTE=$(repoquery --pkgnarrow=all --repofrompath=it4irepo,"$REPOPATH""$ARCH"/ --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$RPM_NAME"."$ARCH")
     if [ -z "$RPM_REMOTE" ]; then
       echo "Brand new package $RPM_LOCAL.$ARCH"
-      continue
-    fi
-    rpmdev-vercmp "$RPM_LOCAL" "$RPM_REMOTE"
-    if [ $? != 11 ]; then
-      echo "Building $RPM_LOCAL.$ARCH is not newer then package in the remote yum repository."
-      exit 1
-    fi
-    RPM_REMOTE=$(repoquery --pkgnarrow=all --releaseserver="$RELEASESERVER" --srpm --queryformat "%{NAME}-%{VERSION}-%{RELEASE}" "$RPM_NAME")
-    if [ -z "$RPM_REMOTE" ]; then
-      echo "Brand new source package $RPM_LOCAL.$ARCH"
-      continue
-    fi
-    rpmdev-vercmp "$RPM_LOCAL" "$RPM_REMOTE"
-    if [ $? != 11 ]; then
-      echo "Building source $RPM_LOCAL.$ARCH is not newer then package in the remote yum repository."
-      exit 1
+    else
+      rpmdev-vercmp "$RPM_LOCAL"."$ARCH" "$RPM_REMOTE"."$ARCH"
+      if [ $? != 11 ]; then
+        echo "Building $RPM_LOCAL.$ARCH is not newer then package in the remote yum repository."
+        exit 1
+      fi
     fi
   done
 done
