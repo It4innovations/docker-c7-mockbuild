ec596dbc79010418992f97966a5b673ad170034a
Marek Chrastina <marek.chrastina@vsb.cz>
2019-05-10 09:43:48 +0200

    Add origin to git diff

--- getspecs.sh
+++ getspecs.sh
@@ -7,6 +7,6 @@ fi
 if [ "$BRANCH" == "master" ]; then
   LIST=$(git log -m -1 --name-only --pretty="format:" | sed '/./,$!d' | awk '/^$/{exit} {print $0}' | sed '/\.spec$/!d')
 else
-  LIST=$(git diff master..."$BRANCH" --name-only | sed '/\.spec$/!d')
+  LIST=$(git diff origin/master...origin/"$BRANCH" --name-only | sed '/\.spec$/!d')
 fi
 echo "$LIST"
